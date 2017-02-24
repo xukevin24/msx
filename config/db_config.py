8d0394ed9cc24500a8cce37c33bfe75fc078d3f2
xuzhifei <xuzhifei@qq.com>
2017-02-24 12:50:13 +0800

    数据库密码从配置文件读取
    需要建立文件c:/db.conf，放入数据库ip和密码
    [db]
    ip = 120.25.123.12
    passwd = 123456

--- config/db_config.py
+++ config/db_config.py
@@ -1,4 +1,15 @@
+import configparser
+
 ip = "127.0.0.1"
 port = 3306
 user = 'root'
 passwd = '123456'
+
+cp = configparser.SafeConfigParser()
+cp.read('c:/db.conf')
+
+ip = cp.get('db', 'ip')
+passwd = cp.get('db', 'passwd')
+
+#print(ip)
+#print(passwd)

7fcb9dcfe1b6fcc76d02664a2f834b8df29916d1
xukevin24 <xukevin24@gmail.com>
2017-03-02 00:49:15 -0500

    xx

--- strategy/test_strategy.py
+++ strategy/test_strategy.py
@@ -9,7 +9,7 @@ if cwd not in sys.path:
 
 import strategy.istrategy as istrategy
 import data_api 
-
+import operator
 class Strategy(istrategy.IStrategy):
     def __init__(self, enterStrategy, exitStrategy):
         self.enter_strategy = enterStrategy
@@ -18,11 +18,25 @@ class Strategy(istrategy.IStrategy):
     #返回最小开始索引
     def min_start(self):
         return max(self.enter_strategy.min_start(), self.exit_strategy.min_start())
+    
+    def logic(a,func,b):
+        mappings={
+                '&': operator.and_,
+                '|': operator.or_
+                }
+        return mappings[func](a,b)
 
     #对某一天返回是否进场点
     def is_entry(self, dataApi, index):
-        return self.enter_strategy.is_entry(dataApi, index)
-
+        if len(self.enterStrategy)==1:
+            return self.enter_strategy[0].is_entry(dataApi, index)
+        else:
+            for s in self.enter_strategy:
+                if type(s)==list:
+                    s=self.logic(s[0].is_entry(dataApi, index),s[1],s[2].is_entry(dataApi, index))
+                else:
+                    continue
+            
     #对某一天返回是否出场
     def is_exit(self, dataApi, index, enterInfo):
         return self.exit_strategy.is_exit(dataApi, index, enterInfo)

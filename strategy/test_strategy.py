e4ec0703056ff83585532af511fd7ef9595db5ab
ax614 <xuzhifei@gmail.com>
2017-03-03 22:16:03 +0800

    Revert "时间出场，百分比亏损出场策略"
    This reverts commit cbc026dd047a076f735e0b327c40f79ddc0425b5.

--- strategy/test_strategy.py
+++ strategy/test_strategy.py
@@ -12,33 +12,20 @@ import data_api
 
 class Strategy(istrategy.IStrategy):
     def __init__(self, enterStrategy, exitStrategy):
-        self.enter_strategy = enterStrategy #[]与
-        self.exit_strategy = exitStrategy   #[]或
-        self.min_start_val = -1
+        self.enter_strategy = enterStrategy
+        self.exit_strategy = exitStrategy
 
     #返回最小开始索引
     def min_start(self):
-        if self.min_start_val < 0:
-            self.min_start_val = 0
-            for STG in self.enter_strategy:
-                self.min_start_val = max(STG.min_start(), self.min_start_val)
-            for STG in self.exit_strategy:
-                self.min_start_val = max(STG.min_start(), self.min_start_val)
-        return self.min_start_val
+        return max(self.enter_strategy.min_start(), self.exit_strategy.min_start())
 
     #对某一天返回是否进场点
     def is_entry(self, dataApi, index):
-        for STG in self.enter_strategy:
-            if STG.is_entry(dataApi, index) == False:
-                return False
-        return True
+        return self.enter_strategy.is_entry(dataApi, index)
 
     #对某一天返回是否出场
     def is_exit(self, dataApi, index, enterInfo):
-        for STG in self.exit_strategy:
-            if STG.is_exit(dataApi, index, enterInfo):
-                return True
-        return False
+        return self.exit_strategy.is_exit(dataApi, index, enterInfo)
 
     #进场使用资金比率
     def get_percent(self):

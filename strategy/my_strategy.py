1d56c906c882daa1e87d08b2a0b6e5ae5db26746
xuzhifei <xuzhifei@qq.com>
2017-03-18 18:15:14 +0800

    xiaoyuan的

--- strategy/my_strategy.py
+++ strategy/my_strategy.py
@@ -14,42 +14,25 @@ class Strategy(istrategy.IStrategy):
         self.N1 = N1
 
     def min_start(self):
-        return 200
+        return self.N1 * 2 + 1
 
     def is_entry(self, dataApi, index):
-        if self.is_high(dataApi, index) and self.is_low(dataApi, index):
-            return True
+        if ((dataApi.open(index) - dataApi.low(index)) / abs(dataApi.open(index) - dataApi.close(index)) <= 2:
+            return False
+        if dataApi.close(index) >= dataApi.close(index + 1):
+            return False
+
+        for i in range(20):
+            if self.is_exist(i + 1):
+                return True
         return False
+
+    def is_exist(n):
+        pass
 
     def is_exit(self, dataApi, index, enterInfo):
+        if dataApi.close(index) < dataApi.close(index + 1) and dataApi.high(index) < dataApi.high(index + 1):
+            return True
         return False
 
-    def is_low(self, dataApi, index):
-        idx1 = dataApi.last_low_point(index, self.N1)
-        if idx1 < 0:
-            return False
-        idx2 = dataApi.last_low_point(idx1, self.N1)
-        if idx1 < 0:
-            return False
-        if dataApi.low(idx1) > dataApi.low(idx2) * 1.2:
-            return True
-        idx3 = dataApi.last_low_point(idx2, self.N1)
-        if idx3 < 0:
-            return False
-        if dataApi.low(idx2) > dataApi.low(idx3) * 1.2:
-            return True
 
-    def is_high(self, dataApi, index):
-        idx1 = dataApi.last_high_point(index, self.N1)
-        if idx1 < 0:
-            return False
-        idx2 = dataApi.last_high_point(idx1, self.N1)
-        if idx1 < 0:
-            return False
-        if dataApi.high(idx1) > dataApi.high(idx2) * 1.2:
-            return True      
-        idx3 = dataApi.last_high_point(idx2, self.N1)
-        if idx3 < 0:
-            return False
-        if dataApi.high(idx2) > dataApi.high(idx3) * 1.2:
-            return True              
